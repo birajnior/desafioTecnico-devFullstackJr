aa09514b22d8ec70fec940970a21c103717e895f
birajnior <96500363+birajnior@users.noreply.github.com>
2025-09-19 23:55:02 -0300

    feat(frontend): concluir implementação do layout das telas públicas e privadas

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+// Lista de produtos
+router.get("/", (req, res) => {
+  res.render("products/list", {
+    title: "Lista de Produtos",
+    produtos: [], // depois vamos puxar do banco
+  });
+});
+
+// Novo produto
 router.get("/new", (req, res) => {
-  res.render("products/new", { title: "Cadastrar Produto" });
+  res.render("products/new", {
+    title: "Cadastrar Produto",
+  });
+});
+
+// Editar produto
+router.get("/:id/edit", (req, res) => {
+  res.render("products/edit", {
+    title: "Editar Produto",
+    produto: { id: req.params.id },
+  });
 });
 
 module.exports = router;
